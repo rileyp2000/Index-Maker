feaaddc15d016ef9eb79379c364aacaa5f157a27
rileyp2000 <rileyp@bishopireton.org>
2017-09-12 22:15:02 -0400

    first edit

--- Index Maker/src/DocumentIndex.java
+++ Index Maker/src/DocumentIndex.java
@@ -2,7 +2,9 @@
  * <p></p>
  * @author Patrick Riley
  */
+
 import java.util.ArrayList;
+
 public class DocumentIndex extends ArrayList<IndexEntry>{
 
 }
--- Index Maker/src/IndexEntry.java
+++ Index Maker/src/IndexEntry.java
@@ -1,12 +1,41 @@
+import java.util.ArrayList;
+
 /**
  * <p></p>
  * @author Patrick Riley
  */
 
-/**
- * @author Patrick
- *
- */
 public class IndexEntry {
+	
+	private String word;
+	private ArrayList<Integer> numsList;
+	
+	/**
+	 * <p>Constructs a new IndexEntry object</p>
+	 * @param wd
+	 */
+	public IndexEntry(String wd){
+		word = wd.toUpperCase();
+		numsList = new ArrayList<Integer>();
+	}
+	
+	/**
+	 * 
+	 * @return String word
+	 */
+	public String getWord() {
+		return word;
+	}
+	
+	public void add(int num){
+		if(!numsList.contains(num))
+			numsList.add(num);
+	}
 
+	@Override
+	public String toString() {
+		return "IndexEntry [word=" + word + ", numsList=" + numsList + "]";
+	}
+	
+	
 }
