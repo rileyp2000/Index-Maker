e6e53b69d653b0a4f4b6e3f3101a0deb8889d064
rileyp2000 <rileyp@bishopireton.org>
2017-09-12 22:17:12 -0400

    s

--- Index Maker/src/DocumentIndex.java
+++ Index Maker/src/DocumentIndex.java
@@ -6,5 +6,24 @@
 import java.util.ArrayList;
 
 public class DocumentIndex extends ArrayList<IndexEntry>{
-
+	
+	public DocumentIndex(){
+		super();
+	}
+	
+	private int foundOrInserted(String word){
+		
+	}
+	
+	public DocumentIndex(int cap){
+		super(cap);
+	}
+	
+	public void addWord(String word, int num){
+		
+	}
+	
+	public void addAllWords(String str, int num){
+		
+	}
 }
