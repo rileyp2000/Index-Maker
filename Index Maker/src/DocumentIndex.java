d85a4c5569f2ad9d3f7d36568ae9ba314b627027
rileyp2000 <rileyp@bishopireton.org>
2017-10-25 13:33:55 -0400

    changed spacing

--- Index Maker/src/DocumentIndex.java
+++ Index Maker/src/DocumentIndex.java
@@ -1,3 +1,4 @@
+
 /**
  * <p>This is the index of the document</p>
  * @author Patrick Riley
@@ -6,67 +7,71 @@
 import java.util.ArrayList;
 
 @SuppressWarnings("serial")
-public class DocumentIndex extends ArrayList<IndexEntry>{
-	
-	public DocumentIndex(){
+public class DocumentIndex extends ArrayList<IndexEntry> {
+
+	public DocumentIndex() {
 		super();
 	}
-	
+
 	/**
 	 * 
-	 * @param cap Capacity
+	 * @param cap
+	 *            Capacity
 	 */
-	public DocumentIndex(int cap){
+	public DocumentIndex(int cap) {
 		super(cap);
 	}
+
 	/**
 	 * 
-	 * @param word word to find if inserted
+	 * @param word
+	 *            word to find if inserted
 	 * @return int index of return
 	 */
-	private int foundOrInserted(String word){
+	private int foundOrInserted(String word) {
 		word = word.toUpperCase();
 		boolean isInserted = false;
-		int i = 0; 
+		int i = 0;
 		String compWord = "";
-		while(i < super.size() && !isInserted) {
+		while (i < super.size() && !isInserted) {
 			compWord = super.get(i).getWord();
-			if(compWord.equals(word))
+			if (compWord.equals(word))
 				return i;
-			else if(compWord.compareTo(word)<0)
-					i++;
+			else if (compWord.compareTo(word) < 0)
+				i++;
 			else {
 				this.add(i, new IndexEntry(word));
 				isInserted = true;
 				return i;
 			}
 		}
-			this.add(new IndexEntry(word));
-			return i;
+		this.add(new IndexEntry(word));
+		return i;
 	}
-	
 
-
-	
 	/**
 	 * 
-	 * @param word add this word
-	 * @param num on this page line
+	 * @param word
+	 *            add this word
+	 * @param num
+	 *            on this page line
 	 */
-	public void addWord(String word, int num){
+	public void addWord(String word, int num) {
 		this.get(foundOrInserted(word)).add(num);
 	}
-	
+
 	/**
 	 * 
-	 * @param str a line of text
-	 * @param num the line number of the text
+	 * @param str
+	 *            a line of text
+	 * @param num
+	 *            the line number of the text
 	 */
-	public void addAllWords(String str, int num){
+	public void addAllWords(String str, int num) {
 		String[] words = str.split("\\W+");
-		for(String s: words) {
-			if(!s.equals(""))
-				addWord(s,num);
+		for (String s : words) {
+			if (!s.equals(""))
+				addWord(s, num);
+		}
 	}
-	}
-	}
+}
